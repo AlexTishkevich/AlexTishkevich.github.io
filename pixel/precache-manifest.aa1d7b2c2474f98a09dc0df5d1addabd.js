eab8b6351fa9638b1a671b284a00d26aae3e9e75
AlexTishkevich <45825000+AlexTishkevich@users.noreply.github.com>
2018-12-19 02:19:03 +0300

    pixel

---
+++ pixel/precache-manifest.aa1d7b2c2474f98a09dc0df5d1addabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b69139c5920d80279efda1d7b2021878",
+    "url": "/AlexTishkevich.github.io/static/media/logo_w.b69139c5.png"
+  },
+  {
+    "revision": "0a8cf2723217ec42fe3a",
+    "url": "/AlexTishkevich.github.io/static/js/runtime~main.0a8cf272.js"
+  },
+  {
+    "revision": "bbc24c8c600d8dc46b00",
+    "url": "/AlexTishkevich.github.io/static/js/main.bbc24c8c.chunk.js"
+  },
+  {
+    "revision": "a27667fd80b0756b3bc4",
+    "url": "/AlexTishkevich.github.io/static/js/1.a27667fd.chunk.js"
+  },
+  {
+    "revision": "bbc24c8c600d8dc46b00",
+    "url": "/AlexTishkevich.github.io/static/css/main.76878224.chunk.css"
+  },
+  {
+    "revision": "13438cda38e8cb55ac6560020972f276",
+    "url": "/AlexTishkevich.github.io/index.html"
+  }
+];
